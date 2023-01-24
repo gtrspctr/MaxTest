a44970bfd5557745e7c81ea2d6dbfb81085841ee
Al Robison <99000777+gtrspctr@users.noreply.github.com>
2023-01-24 11:17:09 -0700

    Delete request_single_key.js

--- https_server/request_single_key.js
+++
@@ -1,8 +0,0 @@
-// POST request handler where a user may request the value for a key
-// NOT WORKING
-var value = prompt("What key would you like to lookup? ");
-
-
-fetch('https://alrobison.com/users')
-    .then(response => response.json())
-    .then(data => console.log(data))
