93161d13bca5e22ba8db23bc3d93f74634b9ba0b
Al <al@Als-MacBook-Air.local>
2023-01-25 14:36:23 -0700

    Editing scripts

--- https_server/scripts/get_data.py
+++ https_server/scripts/get_data.py
@@ -4,8 +4,8 @@ import json
 url = "https://alrobison.com/users"
 req = requests.get(url)
 json_data = json.loads(req.text)
-
 total_items = len(json_data)
+
 obj_attributes = []
 total_attributes = len(obj_attributes)
 
@@ -39,35 +39,62 @@ class User:
 #    def create_user(cls):
 #        return cls(**json_data)
 
+# Take json dicts and create python objects
 user_list = []
 for data in json_data:
     user_list.append(User(**data))
 
+# Get user object attributes
+# Will use these for requests
 for key, value in user_list[0].__dict__.items():
 	obj_attributes.append(key)
 	total_attributes += 1
 
+# clearScreen() simply prints a blank line 50 times
+# to help keep these easy to read.
+def clearScreen():
+	for i in range(50):
+		print()
+
+# parseIntAndRange() attempts to change a string into an integer.
+# If successful, it checks if integer is between min and max.
+# if unsuccessful, it will catch an error and return false
 def parseIntAndRange(choice, min, max):
 	try:
 		choice = int(choice)
 		if isinstance(choice, int) and choice >= min and choice <= max:
+			# choice is an integer and it's in the correct range
 			return True
 		else:
+			# choice is an integer but NOT in the correct range
 			return False
 	except:
+		# choice cannot be turned into an integer
 		return False
 
+# requestAll() simply prints a dump of all json_data,
+# with each dict separated by a blank space for
+# readability.
 def requestAll():
 	for item in json_data:
 		print(item)
 		print()
 
+# requestValue() asks the user which json dict they
+# would like to update. It lists the 'names' of the
+# dicts.
+# It uses a while loop and parseIntAndRange() to verify
+# input is valid.
+# Then asks the user which value they would like to 
+# update by giving them the keys for the specified
+# user. Again, uses while loop and parseIntAndRange()
+# It then asks the user for the new value.
+# Then uses all the user input to figure out which
+# object/attribute to change, and updates the value.
 def requestValue():
-	# Clear screen
-	for i in range(50):
-		print()
+	clearScreen()
 
-	# Ask user which data point to update
+	# Ask user which object to update
 	data_choice = ""
 	while True:
 		print("Which user would you like to update?")
@@ -115,18 +142,25 @@ def requestValue():
 
 	print()
 	print()
+	
+	attr = obj_attributes[key_choice-1]
+	#print(str(user_list[data_choice-1]).attr)
+	print(str(user_list[data_choice-1]).__dir__[key_choice-1])
+	print("")
 
+def changeValue():
+	## USE ABOVE FUNCTION HERE
 	# Ask user for updated value
 	value_choice = input("Enter the updated value: ")
 
-	# Whichever number user chooses, minus by 1 for list index
+	# Whichever number user chooses, minus by 1 for array index
 	attr = obj_attributes[key_choice-1]
-	print(attr)
-	print(str(user_list[data_choice-1]) + "." + str(attr) + " = " + value_choice)
+	print()
+	print("Old value: " + str(attr))
 	user_list[data_choice-1].attr = value_choice
+	print("New value: " + value_choice)
 
-def changeValue():
-	pass
+	# NEED TO POST HERE
 
 def deletePair():
 	pass
